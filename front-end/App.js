ed8c1646361efd8dfca692670e80a66492dacf0e
chadkeeven <chadkeeven@gmail.com>
2018-02-25 12:36:13 -0700

    aws mobile things

--- front-end/App.js
+++ front-end/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+
+
+import awsmobile from './aws-exports';
+
+
+Amplify.configure(awsmobile);
 
 export default class App extends React.Component {
   render() {
@@ -8,7 +15,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hello Jin!</Text>
+        <Text>Hello Mark!</Text>
       </View>
     );
   }
