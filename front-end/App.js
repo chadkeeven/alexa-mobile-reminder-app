5e928e819318722494f5ffe5395f3e2776167e11
chadkeeven <chadkeeven@gmail.com>
2018-02-25 17:11:00 -0700

    kinda restarting

--- front-end/App.js
+++ front-end/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Amplify from 'aws-amplify';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import Amplify, { API } from 'aws-amplify';
 
 
 import awsmobile from './aws-exports';
@@ -9,23 +9,73 @@ import awsmobile from './aws-exports';
 Amplify.configure(awsmobile);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hello Mark!</Text>
-      </View>
-    );
-  }
+ constructor(props) {
+  super(props); 
+  state = {
+    apiResponse: null,
+    noteId: '',
+   // text: 'reminder placeholder'
+  };  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+handleChangeNoteId = (event) => {
+  this.setState({noteId: event});
+}
+
+  // saveReminder = () => {
+  //   console.log(this.state.text);
+
+  // }
+// Create a new Note according to the columns we defined earlier
+async saveNote() {
+  //console.log(this.state.text);
+  let newNote = {
+    body: {
+      "NoteTitle": "hi chad",
+      "NoteContent": "This is so cool!",
+      "NoteId": state.noteId
+    }
+  }
+  const path = "/Notes";
+
+      // Use the API module to save the note to the database
+      try {
+        const apiResponse = await API.put("NotesCRUD", path, newNote);
+        console.log("response from saving note: " + apiResponse);
+        //this.setState({apiResponse});
+      } catch (e) {
+        console.log();
+        console.log("Error: " + e);
+      }
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello Chad!</Text>
+        <Text>Reminders:</Text>
+        <TextInput
+        onChangeText={(text) => this.setState({text})}
+        value={state.text}/>
+        <Button
+        onPress={this.saveNote}
+        title="Save"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+        />
+        </View>
+        );
+      }
+    }
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
